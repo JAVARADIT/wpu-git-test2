2a043ac4a0a35bfb8bb341af6e66bee942275d6b
JAVARADIT <ipiaa72@unpas.ac.id>
2026-04-28 22:55:39 +0700

    menambahkan paragraf

--- index.php
+++ index.php
@@ -7,5 +7,6 @@
 </head>
 <body>
     <h1>Helo wolrd</h1>
+    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tempora voluptatem ab labore consequatur, ipsum facere delectus possimus fuga, sequi consectetur quisquam laudantium ipsa unde laborum, suscipit sunt perferendis atque voluptatum.</p>
 </body>
 </html>
